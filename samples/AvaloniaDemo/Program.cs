7fcf79a55528a1a71ad6b978164e9e528584961e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-02-19 21:33:24 +0100

    Added BuildAvaloniaApp

--- samples/AvaloniaDemo/Program.cs
+++ samples/AvaloniaDemo/Program.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Avalonia;
-using Avalonia.Controls;
 using Avalonia.Logging.Serilog;
-using Serilog;
 using System;
 using System.Diagnostics;
 
@@ -11,30 +9,22 @@ namespace AvaloniaDemo
 {
     internal class Program
     {
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                         .UsePlatformDetect()
+                         .LogToDebug();
+
         private static void Main(string[] args)
         {
             try
             {
-                InitializeLogging();
-                AppBuilder.Configure<App>()
-                    .UsePlatformDetect()
-                    .Start<MainWindow>();
+                BuildAvaloniaApp().Start<MainWindow>();
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
                 Debug.WriteLine(ex.StackTrace);
             }
-        }
-
-        private static void InitializeLogging()
-        {
-#if DEBUG
-            SerilogLogger.Initialize(new LoggerConfiguration()
-                .MinimumLevel.Warning()
-                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
-                .CreateLogger());
-#endif
         }
     }
 }
