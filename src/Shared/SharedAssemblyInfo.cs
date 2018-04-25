2900aa53cbce23bfb2a102c2b9929a1e325d9eed
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-04-25 17:27:14 +0200

    Updated version

--- src/Shared/SharedAssemblyInfo.cs
+++ src/Shared/SharedAssemblyInfo.cs
@@ -3,8 +3,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Wiesław Šoltés")]
-[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2018")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
