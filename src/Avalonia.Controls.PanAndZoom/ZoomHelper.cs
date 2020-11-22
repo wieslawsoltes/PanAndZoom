ea1a58b6200da2354e90553aba6d62e2b345fe55
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-22 21:25:40 +0100

    Update ZoomHelper.cs

--- src/Avalonia.Controls.PanAndZoom/ZoomHelper.cs
+++ src/Avalonia.Controls.PanAndZoom/ZoomHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using static System.Math;
 
 namespace Avalonia.Controls.PanAndZoom
@@ -20,35 +21,69 @@ namespace Avalonia.Controls.PanAndZoom
         {
             var transformed = bounds.TransformToAABB(matrix);
 
+            Debug.WriteLine($"bounds: {bounds}, transformed: {transformed}");
+            
             var width = transformed.Size.Width;
             var height = transformed.Size.Height;
 
             if (width < bounds.Width)
             {
                 width = bounds.Width;
+
+                if (transformed.Position.X < 0.0)
+                {
+                    width += Abs(transformed.Position.X);
+                }
+                else
+                {
+                    var widthTranslated = transformed.Size.Width + transformed.Position.X;
+                    if (widthTranslated > width)
+                    {
+                        width += widthTranslated - width;
+                    }
+                }
+            }
+            else
+            {
+                width += Abs(transformed.Position.X);
             }
             
             if (height < bounds.Height)
             {
                 height = bounds.Height;
+                
+                if (transformed.Position.Y < 0.0)
+                {
+                    height += Abs(transformed.Position.Y);
+                }
+                else
+                {
+                    var heightTranslated = transformed.Size.Height + transformed.Position.Y;
+                    if (heightTranslated > height)
+                    {
+                        height += heightTranslated - height;
+                    }
+                }
+            }
+            else
+            {
+                height += Abs(transformed.Position.Y);
             }
 
-            var ex = matrix.M31;
-            var ey = matrix.M32;
-
-            extent = new Size(
-                width + Math.Abs(ex),
-                height + Math.Abs(ey));
+            extent = new Size(width, height);
 
             viewport = bounds.Size;
      
-            var ox = matrix.M31 * matrix.M11;
-            var oy = matrix.M32 * matrix.M22;
+            var ox = transformed.Position.X;
+            var oy = transformed.Position.Y;
 
             var offsetX = ox < 0 ? Abs(ox) : 0;
             var offsetY = oy < 0 ? Abs(oy) : 0;
 
             offset = new Vector(offsetX, offsetY);
+
+            Debug.WriteLine($"Extent: {extent} | Offset: {offset} | Viewport: {viewport}");
+
         }
     }
 }
