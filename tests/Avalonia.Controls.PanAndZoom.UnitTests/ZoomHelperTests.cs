4817eba3dfb511efcba9c14fc4f587fe8ccf3a3d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-22 18:38:59 +0100

    Create ZoomHelperTests.cs

---
+++ tests/Avalonia.Controls.PanAndZoom.UnitTests/ZoomHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Avalonia.Controls.PanAndZoom.UnitTests
+{
+    public class ZoomHelperTests
+    {
+        private Matrix CreateMatrix(double scaleX = 1.0, double scaleY = 1.0, double offsetX = 0.0, double offsetY = 0.0)
+        {
+            return new Matrix(scaleX, 0, 0, scaleY, offsetX, offsetY);
+        }
+        
+        [Fact]
+        public void CalculateScrollable_Defaults()
+        {
+            var bounds = new Rect(0, 0, 100, 100);
+            var matrix = CreateMatrix();
+            ZoomHelper.CalculateScrollable(bounds, matrix, out var extent, out var viewport, out var  offset);
+            Assert.Equal(new Size(100, 100), extent);
+            Assert.Equal(new Size(100, 100), viewport);
+            Assert.Equal(new Vector(0, 0), offset);
+        }
+    }
+}
