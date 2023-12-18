68f7029afcc0720fd933dec20998f77098961e3d
wieslawsoltes <wieslawsoltes@users.noreply.github.com>
2023-12-18 09:54:26 +0000

    Deploying to gh-pages from @ wieslawsoltes/PanAndZoom@45e0d565134a822965ac7d7df769f7aeeb8218d1 🚀

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { dotnet } from './dotnet.js'
+import { registerAvaloniaModule } from './avalonia.js';
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -7,6 +8,8 @@ const dotnetRuntime = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
+
+await registerAvaloniaModule(dotnetRuntime);
 
 const config = dotnetRuntime.getConfig();
 
